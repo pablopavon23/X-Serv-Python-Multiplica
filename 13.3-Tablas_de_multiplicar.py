a238ab209d663b8ef7c1032793eee8b3504a29b7
pablopavon23 <p.pavon23695@gmail.com>
2017-01-30 18:40:24 +0100

    Ejercicio 13.3

---
+++ 13.3-Tablas_de_multiplicar.py
@@ -0,0 +1,7 @@
+for num in range(1,11):
+	print("--------------------------------");
+	print("La tabla del " + str(num) + " es:");
+	print("--------------------------------");
+	for num2 in range(1,11):
+		print(str(num) + " por " + str(num2) + " es:" + str(num*num2));
+		
